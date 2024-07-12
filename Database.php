2203836c3a3086be193a4bc8ca14263ccd21887a
Seif Ahmed <seifahmeed9@gmail.com>
2024-07-13 01:51:45 +0300

    Refactoring database,router,index

--- Database.php
+++ Database.php
@@ -14,11 +14,12 @@ class Database
         $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
 
         $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
 
         try {
-            $this->conn = new PDO($dsn, $config['username'], $config['password']);
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
         } catch (PDOException $e) {
             throw new Exception("Database connection failed: {$e->getMessage()}");
         }
--- public/index.php
+++ public/index.php
@@ -1,16 +1,17 @@
 <?php
 require '../helpers.php';
-require basePath('Database.php');
-$config = require basePath('config/db.php');
-
-$db = new Database($config);
-
 require basePath('Router.php');
+require basePath('Database.php');
 
+// Instantiate the router
 $router = new Router();
+
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get current URI and HTTP methods
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
