6aa1c07b2909b7110e44868c5e218f6692f404d7
Seif Ahmed <seifahmeed9@gmail.com>
2024-07-12 03:23:16 +0300

    Add inspect helper functions

--- helpers.php
+++ helpers.php
@@ -35,3 +35,27 @@ function loadPartial($name)
     if (file_exists($partialPath)) require $partialPath;
     else echo "Partial '{$path}' not found!";
 }
+
+/**
+ * Inspect a value(s)
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+}
