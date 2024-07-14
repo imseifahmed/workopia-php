71bcf5264f84846f33289edefef783773ac8dba0
Seif Ahmed <seifahmeed9@gmail.com>
2024-07-15 02:22:44 +0300

    ErrorController class

--- Framework/Router.php
+++ Framework/Router.php
@@ -2,6 +2,7 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
 class Router
 {
     protected $routes = [];
@@ -75,18 +76,6 @@ class Router
     }
 
     /**
-     * Load error page
-     * @param int httpCode
-     * @return void
-     */
-    public function error($httpCode = 404)
-    {
-        http_response_code($httpCode);
-        loadView("error/{$httpCode}");
-        exit;
-    }
-
-    /**
      * Route the request
      * @param string $uri
      * @param string $method 
@@ -107,6 +96,6 @@ class Router
             }
         }
 
-        $this->error(404);
+        ErrorController::notFound('Resource not found');
     }
 }
