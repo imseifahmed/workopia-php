ca66cf24bbbe11dcadcaefcab578ff2ea5ba422e
Seif Ahmed <seifahmeed9@gmail.com>
2024-07-15 03:01:22 +0300

    Handling route params

--- public/index.php
+++ public/index.php
@@ -17,7 +17,6 @@ $routes = require basePath('routes.php');
 
 // Get current URI and HTTP methods
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
 
 // Route the request
-$router->route($uri, $method);
+$router->route($uri);
--- routes.php
+++ routes.php
@@ -2,5 +2,5 @@
 
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
-$router->get('/listing', 'ListingController@show');
 $router->get('/listings/create', 'ListingController@create');
+$router->get('/listing/{id}', 'ListingController@show');
