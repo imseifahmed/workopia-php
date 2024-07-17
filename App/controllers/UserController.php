dbb8648a8d2d194dfe604cef423971e1ff23ef39
Seif Ahmed <seifahmeed9@gmail.com>
2024-07-18 00:49:58 +0300

    Register user

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -34,4 +34,73 @@ class UserController
     {
         loadView('users/create');
     }
+
+    /**
+     * Store user in database
+     * 
+     * @return void
+     */
+    public function store()
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $password = $_POST['password'];
+        $password_confirmation = $_POST['password_confirmation'];
+
+        $errors = [];
+
+        // Validation
+        if (!Validation::email($email)) $errors['email'] = 'Please enter a valid email address';
+
+        if (!Validation::string($name, 2, 50)) $errors['name'] = 'Please enter a valid email address';
+
+        if (!Validation::string($name, 2, 50)) $errors['name'] = 'Name must be between 2 and 50 characters';
+
+        if (!Validation::string($password, 6, 50)) $errors['password'] = 'Password must be at least 6 characters';
+
+        if (!Validation::match($password, $password_confirmation)) $errors['password_confirmation'] = 'Passwords do not match';
+
+        if (!empty($errors)) {
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state,
+                ]
+            ]);
+            exit;
+        }
+
+        // Check if email exists
+        $params = [
+            'email' => $email,
+        ];
+
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
+
+        if ($user) {
+            $errors['email'] = 'That email already exists';
+            loadView('users/create', [
+                'errors' => $errors
+            ]);
+            exit;
+        }
+
+        // Create user account
+        $params = [
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        ];
+
+        $this->db->query('INSERT INTO users(name, email, city, state, password) VALUES(:name, :email, :city, :state, :password)', $params);
+
+        redirect('/');
+    }
 }
